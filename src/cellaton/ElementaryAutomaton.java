ccd072fcba65c3a807ca25ab011794384f32d19f
Woodbin <woodbin@centrum.cz>
2014-12-01 00:14:01 +0100

    ElementaryAutomaton work

---
+++ src/cellaton/ElementaryAutomaton.java
@@ -0,0 +1,73 @@
+package cellaton;
+
+import cellaton.util.Perceptron;
+import cellaton.util.TeachingPattern;
+
+/**
+ * Created by Woodbin on 30.11.2014.
+ */
+public class ElementaryAutomaton {
+
+    //TODO Elemental Automaton
+
+    private int[] space;
+    private int rule;
+    private Perceptron processer;
+    private boolean perceptronReady;
+    private boolean bordersContinuous;
+
+    public ElementaryAutomaton(){
+        new ElementaryAutomaton(256,90,false);           //basic elementary automaton with rule 90 on 256 size field
+    }
+
+    private ElementaryAutomaton(int _size, int _rule, boolean _bordersContinuous){
+        space = new int[_size];
+        rule = _rule;
+        bordersContinuous = _bordersContinuous;
+
+        processer = new Perceptron();
+        processer.teach(ruleToPatterns(rule));
+        perceptronReady = true;
+
+
+    }
+
+    private TeachingPattern ruleToPatterns(int _rule){
+        TeachingPattern pc = new TeachingPattern(8,4);
+
+        //TODO make converting from rule to perceptron pattern
+
+        return pc;
+    }
+
+    public void generate(){
+        if(perceptronReady){
+            int size = space.length;
+            int[] newGeneration = new int[size];
+            if(bordersContinuous){
+                for(int i = size-1; i<size*2;i++){
+                    int[] cells = {space[(i-1)%size],space[i%size],space[(i+1)%size]};
+                    newGeneration[i%size] = processer.respond(cells);
+                }
+            }
+            if(!bordersContinuous){
+                int[] cells = {space[0],space[1],space[2]};
+                newGeneration[0]= processer.respond(cells);
+                for(int i = 1; i<size-1;i++) {
+                    cells = new int[]{space[i-1],space[i],space[i+1]};
+                    newGeneration[i]=processer.respond(cells);
+
+                }
+                cells = new int[]{space[size-3],space[size-2], space[size-1]};
+                newGeneration[size-1] = processer.respond(cells);
+            }
+            space = newGeneration;
+        }
+    }
+
+
+
+
+
+
+}
