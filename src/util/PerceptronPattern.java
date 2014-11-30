e42910bf91b2051a4038e3bcd19b28a17252c40c
Woodbin <woodbin@centrum.cz>
2014-11-30 22:22:53 +0100

    Created perceptronpattern class for perceptron teaching

---
+++ src/util/PerceptronPattern.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Created by Woodbin on 30.11.2014.
+ * Crate class for perceptron teaching
+ */
+public class PerceptronPattern {
+    public int[] ys;
+    public int[][] X;
+
+    
+
+
+}
