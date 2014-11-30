f515daf3652004fe25dd35888b6b9a4aae1068a0
Woodbin <woodbin@centrum.cz>
2014-11-30 21:46:50 +0100

    DebugCore work

--- src/DebugCore.java
+++ src/DebugCore.java
@@ -1,5 +1,42 @@
+import util.ErrorCode;
+
 /**
  * Created by Woodbin on 30.11.2014.
  */
 public class DebugCore {
+
+    private static DebugCore instance = new DebugCore();
+    private static boolean consoleExists;
+
+
+    public static DebugCore getInstance() { return instance;}
+
+    public static void debugOut(String message) {
+        processIn(message);
+    }
+
+    public static void debugIn(String command) {
+        processOut(command);
+    }
+
+    public static void processOut(String message){
+        //TODO console message processing
+    }
+
+    public static void processIn(String message){
+        //TODO console command processing
+    }
+
+    public static String getErrorMessage (ErrorCode errorCode){
+        String mes = new String();
+
+        //TODO ErrorCode generation
+
+        return mes;
+    }
+
+
+
+
+
 }
---
+++ src/util/ErrorCode.java
@@ -0,0 +1,11 @@
+package util;
+
+/**
+ * Created by Woodbin on 30.11.2014.
+ */
+public enum ErrorCode {
+    OKAY,
+
+
+
+}
