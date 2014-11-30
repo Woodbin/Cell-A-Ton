763bc2189692d36174cbc2b99debe8f5f8256d8d
Woodbin <woodbin@centrum.cz>
2014-11-30 22:40:59 +0100

    created respond method in Perceptron class

--- src/cellaton/util/Perceptron.java
+++ src/cellaton/util/Perceptron.java
@@ -7,18 +7,19 @@ public class Perceptron {
 
     private int[] weights;
     private int threshold;
-    private static DebugCore debug = DebugCore.getInstance();
 
 
     public int respond(int[] input){
         int ret = 0;
         if(input.length!=weights.length){
-
+            DebugCore.debugOut("Lengths of perceptron inputs and weights doesn't equal!");
         }
         else {
             for (int i = 0; i < weights.length; i++) {
-
+                ret += input[i]*weights[i];
             }
+            ret+=threshold;
+            ret = (int) Math.signum(ret);
         }
         return ret;
     }
