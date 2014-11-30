417990b22b051cfdb94c4d1671213e0a40d7f012
Woodbin <woodbin@centrum.cz>
2014-11-30 21:50:21 +0100

    Created Parser util class

---
+++ src/util/Parser.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by Woodbin on 30.11.2014.
+ */
+public class Parser {
+    private static Parser ourInstance = new Parser();
+
+    public static Parser getInstance() {
+        return ourInstance;
+    }
+
+    private Parser() {
+    }
+
+    public ArrayList<Integer> stringToInts(String s){
+        ArrayList<Integer> numera = new ArrayList<Integer>();
+        for(int i = 0; i<s.length(); i++){
+
+            int temp=Character.getNumericValue(s.charAt(i));
+            if((temp>=0)&&(temp<=8)) numera.add(temp);
+        }
+
+        return numera;
+    }
+
+    public ArrayList<Rule> streamToRules(FileInputStream fis){
+        ArrayList<Rule> ret = new ArrayList<Rule>();
+        //TODO - XML parsing?
+
+        return ret;
+    }
+
+
+
+}
