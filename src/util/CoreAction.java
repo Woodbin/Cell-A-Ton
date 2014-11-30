8ae5b7203850cb9e6c5e8b0d77269b295478916b
Woodbin <woodbin@centrum.cz>
2014-11-30 21:53:22 +0100

    Created CoreAction enum

---
+++ src/util/CoreAction.java
@@ -0,0 +1,17 @@
+package util;
+
+/**
+ * Created by Woodbin on 30.11.2014.
+ */
+public enum CoreAction {
+    ITERATE,
+    NEWRULE,
+    CLEAR,
+    UNDO,
+    LOAD,
+    SAVE,
+    EXPORT,
+    IMPORT,
+
+
+}
