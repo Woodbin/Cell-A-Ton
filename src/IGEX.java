8869ea6b240e219a18cd4cc3818c3f90622d572c
Woodbin <woodbin@centrum.cz>
2014-11-30 22:08:46 +0100

    Created IGEX class

---
+++ src/IGEX.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Woodbin on 30.11.2014.
+ * Image and Gif EXporter
+ * Yes, this will be improved and more versatile version of GEX from MGoL
+ */
+public class IGEX {
+
+
+
+    //TODO IGEX
+
+
+}
