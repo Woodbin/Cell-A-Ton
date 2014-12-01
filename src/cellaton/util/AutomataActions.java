19794e0faa3bc51a4837d378e0498bb097b72ad8
Woodbin <woodbin@centrum.cz>
2014-12-01 01:08:17 +0100

    Created AutomataActions interface for future core use

---
+++ src/cellaton/util/AutomataActions.java
@@ -0,0 +1,18 @@
+package cellaton.util;
+
+/**
+ * Created by Woodbin on 1.12.2014.
+ */
+public interface AutomataActions {
+
+    public void iterate();
+
+    public void clear();
+
+    public void undo();
+
+
+
+
+
+}
